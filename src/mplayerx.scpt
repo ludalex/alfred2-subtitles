29483b1c043fc5b6d73fe779bfdf744291e94bd1
ludalex-ntt <luca.dalessandro@ntt.ie>
2013-10-11 17:12:12 +0200

    #1 Release

---
+++ src/mplayerx.scpt
@@ -0,0 +1,3 @@
+tell application "System Events" to tell application process "MPlayerX"
+	title of window 1
+end tell
---
+++ src/search.php
@@ -0,0 +1,130 @@
+<?
+
+function GetBetween($content,$start,$end){
+    $r = explode($start, $content);
+    if (isset($r[1])){
+        $r = explode($end, $r[1]);
+        return $r[0];
+    }
+    return '';
+}
+
+function remove_nbsp($src) {
+    return str_replace("&nbsp;", "", $src);
+}
+
+include("simple_html_dom.php");
+include("workflows.php");
+
+error_reporting(0);
+
+$w = new Workflows();
+$home = $w->home();
+$path = $w->path();
+
+$host = 'addic7ed.com'; 
+$port = 80; 
+$waitTimeoutInSeconds = 5; 
+
+if($fp = fsockopen($host,$port,$errCode,$errStr,$waitTimeoutInSeconds))
+{   
+
+      $res = exec('osascript -e "tell application \"VLC\" to name of current item"');
+
+      if (!$res) { 
+
+        $script = "mplayerx.scpt";
+        $res = exec("osascript '$script' 2>&1", $output, $error);  
+        // echo "res: " . $res . "\n\n";
+        // echo "output: " . $output . "\n\n";
+        // echo "error: " . $error . "\n\n";
+
+        if($res && !$error)
+          $app = "MPlayerX";
+        else
+          {
+            $w->result( 'alfred', 'alfredapp', "Can't detect any active players.", "Video players supported: VLC, MPlayerX", 'fileicon:/Applications/Alfred.app', 'yes', 'Alfredapp' );
+            echo $w->toxml();
+            die();
+          }
+      }
+      else
+      {
+        $app = "VLC";
+      }
+
+
+      // // exec("osascript -e 'set the clipboard to \"asdasdciao\"' ");
+      $preliminary = $res;
+      $fileName = str_replace(" ", "_", $res);
+      // $friendly_query = str_replace("search", replace, subject)
+
+      preg_match('/^\D*(?=\d)/', $preliminary, $m);
+      $firstNumberPos =  strlen($m[0]);
+      if (!is_numeric( $preliminary[$firstNumberPos+1] )) $preliminary[$firstNumberPos-1] = "0";
+
+      $preliminary = str_replace(".mp4", "", $preliminary);
+      $preliminary = str_replace(".avi", "", $preliminary);
+      $preliminary = str_replace(".mkv", "", $preliminary);
+      $preliminary = str_replace(".mkv", "", $preliminary);
+      $preliminary = str_replace("-", "", $preliminary);
+
+      $query = utf8_encode(urlencode($preliminary));
+      $url = "https://www.google.com/search?q=site%3Aaddic7ed.com+inurl%3Aserie+-inurl%3Aarchives+-inurl%3Ablog+$query";
+
+
+      //exec("osascript -e 'set the clipboard to \"".$url."\"' ");
+
+      $html1 = file_get_html( $url );
+      $found = FALSE;
+      foreach($html1->find('h3[class="r"]') as $element)
+      {
+      	$addic7ed_link = GetBetween($element, 'href="/url?q=', '"') . '<br>'; 
+      	if($addic7ed_link) {         
+          $found = TRUE;
+          break;
+        }
+      }
+      if(!$found) 
+        $w->result( 'alfred', 'alfredapp', "No subtitles found (on Google).", "", 'fileicon:/Applications/Alfred.app', 'yes', 'Alfredapp' );
+
+      exec("osascript -e 'set the clipboard to \"".$url."\"' ");
+
+      $html2 = file_get_html( $addic7ed_link );
+
+      $found = FALSE;
+      foreach($html2->find('div[id=container95m]') as $element)
+      {
+         $found = TRUE;
+
+         $hreff = $element->find('a[class=buttonDownload]');
+         $finalLink =  "http://www.addic7ed.com". $hreff[0]->href;
+
+         $releaser = $element->find('td[class=NewsTitle]');
+         $releaser = trim(remove_nbsp($releaser[0]->plaintext));
+
+         $language = $element->find('td[class=language]');
+         $lanaguage = $language[0];
+         $language = trim(remove_nbsp($language[0]->plaintext));
+
+         //echo();
+
+         $notes = $element->find('td[class=newsDate]');
+         $notes = trim(remove_nbsp($notes[0]->plaintext));
+
+         if($hreff) 
+            $w->result( 'alfred', $app."|".$releaser."|".$finalLink."|".$fileName, $language . " - " . $releaser, $notes, 'fileicon:'.$path.'/icon.png', 'yes', 'Alfredapp' );
+      }
+      if(!$found) 
+        $w->result( 'alfred', 'alfredapp', "No subtitles found (on addic7ed).", "", 'fileicon:/Applications/Alfred.app', 'yes', 'Alfredapp' );
+
+    // exec("osascript -e 'tell application \"VLC\" to open \"$dl_locale\"'");
+
+} else {
+
+    $w->result( 'alfred', 'alfredapp', "Either your Internet connection or Addic7ed are down.", "", 'fileicon:/Applications/Alfred.app', 'yes', 'Alfredapp' );
+
+} 
+      //$w->result( 'alfred', 'alfredapp', "Addic7ed appears to be down.", "", 'fileicon:/Applications/Alfred.app', 'yes', 'Alfredapp' );
+
+echo $w->toxml();
